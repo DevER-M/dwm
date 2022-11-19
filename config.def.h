2d8b453960c1e8f9ad5dd381950bff6cb9a05fe6
DevER-M <sankam.mithunganeshviib@gmail.com>
2022-11-19 07:37:50 +0530

    added flameshot keybind

--- config.def.h
+++ config.def.h
@@ -90,6 +90,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  { MODKEY,                       XK_p,      spawn,          SHCMD("flameshot gui -s -c && killall flameshot")},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
